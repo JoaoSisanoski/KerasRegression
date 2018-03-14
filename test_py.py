d614d5b17ecdd9b019a2171637580f9209f4dbf5
Joao Sisanoski <joaosisanoski@live.com>
2018-03-14 00:30:37 -0300

    adding files

---
+++ test_py.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+dataframe = pd.read_csv("housing.csv", header=None)
+dataset = dataframe.values
+# split into input (X) and output (Y) variables
+X = dataset[:,0:13]
+Y = dataset[:,13]
+seed = 7
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# define wider model
+def wider_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(output_dim = 7, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(output_dim = 7, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
+
+regressor = KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5)
+parameters = {'batch_size': [5, 32, 64], 'epochs': [100, 500]}
+grid_search = GridSearchCV(estimator=regressor,
+                           param_grid=parameters,
+                           scoring='neg_mean_squared_error',
+                           cv=10)
+grid_search = grid_search.fit(X, Y)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+print best_parameters, best_accuracy
